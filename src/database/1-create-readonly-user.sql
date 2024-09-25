5a45dc707388dc8a1cd0d0c363df433792272099
22kHertz <noah.giger@aris-space.ch>
2024-09-25 11:59:17 +0200

    Update 1-create-readonly-user.sql

--- src/database/1-create-readonly-user.sql
+++ src/database/1-create-readonly-user.sql
@@ -3,6 +3,6 @@
 -- [WARNING!]
 -- Make sure to update the password before running the SQL database setup with the password mentioned in the wiki.
 -- Otherwise the server setup is highly insecure and susceptible to attacks potentially resulting in unavailability and dataloss!
-CREATE USER 'aris-read-only-user'@'%' IDENTIFIED BY 'Replace_me_wh3n_deploying_on_public_server';
+CREATE USER 'aris-read-only-user'@'%' IDENTIFIED BY 't2NxM9FTCeK8neSZJq5CSe';
 GRANT SELECT ON aris.* TO 'aris-read-only-user'@'%';
 FLUSH PRIVILEGES;
